f989c5a61d296c63affb37164dde2210b07ed672
THANTOEAUNG <kyawshein034@gmail.com>
2024-04-23 23:21:15 +0900

    change

--- src/component/Nav-bar.jsx
+++ src/component/Nav-bar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col items-center justify-center">
       <NavLink className=" hover:bg-black hover:text-white p-3 w-full" to={"/"}>Product</NavLink>
       <NavLink className="hover:bg-black hover:text-white p-3 w-full" to={"about"}>About</NavLink>
     </div>
